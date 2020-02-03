2976be90842797a676b65ad3eb3b53ab8fe53fba
Deval Patel <dp428@njit.edu>
2020-02-03 17:08:49 -0500

    Adding Homework

---
+++ DBsetup/check_db.php
@@ -0,0 +1,27 @@
+<?php
+//this is check_db.php
+require("config.php");
+echo "DBuser: " . $dbuser;
+echo "\n\r";
+
+$connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
+
+try{
+	$db = new PDO($connection_string, $dbuser, $dbpass);
+	echo  "Should have connected";
+	$stmt = $db->prepare("CREATE TABLE `Test` (
+			  	`id` int auto_increment not null,
+				`username` varchar(30) not null unique,
+				`pin` int default 0,
+				PRIMARY KEY (`id`)
+				) CHARACTER SET utf8 COLLATE utf8_general_ci"
+			);
+	$stmt->execute();
+	echo var_export($stmt->errorInfo(), true);
+}
+catch(Exception $e){
+	echo $e->getMessage();
+	exit("It didn't work");
+}
+
+?>
---
+++ DBsetup/config.sample.php
@@ -0,0 +1,6 @@
+<?
+$dbuser = "dp428";
+$dppass = "Davidptl1996!";
+$dbhost = "sql1.njit.edu";
+$dbdatabase = "dp248"	
+?>
